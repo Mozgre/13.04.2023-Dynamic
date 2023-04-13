f3e2c3518adc2eb72ce46210e3bfee905ed3e8fa
Mozgre <gaz5328@gmail.com>
2023-04-13 09:35:16 +0300

    init

---
+++ Dynamic Language Runtime Кудряшов Александр 1-1П11/Gryppa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Gryppa
+    {
+        private readonly string name;
+        public string Name { get => name; }
+
+        private readonly string shortname;
+        public string Shortname { get => shortname; }
+
+        private readonly byte chislennost;
+        public byte Chislennost { get => chislennost; }
+
+        private readonly short yearPostyp;
+        public short YearPostyp { get => yearPostyp; }
+
+        public Gryppa(string name, string shortname, byte chislennost, short yearPostyp)
+        {
+            this.name = name;
+            this.shortname = shortname;
+            this.chislennost = chislennost;
+            this.yearPostyp = yearPostyp;
+        }
+    }
+}
---
+++ Dynamic Language Runtime Кудряшов Александр 1-1П11/Program.cs
@@ -0,0 +1,50 @@
+﻿// Net 6.0
+using System.Dynamic;
+dynamic student = new ExpandoObject();
+Console.Write("Введите фамилию: ");
+student.Surname = Console.ReadLine() ?? "Нет значения";
+Console.Write("Имя: ");
+student.Name = Console.ReadLine() ?? "Нет значения";
+Console.Write("Отчество: ");
+student.Shortname = Console.ReadLine() ?? "Нет значения";
+//Ссылка на группу->где-то тут
+Console.Write("Дату рождения: ");
+student.Data = Console.ReadLine() ?? "Нет значения";
+Console.WriteLine("\nДанные студента: ");
+foreach (var znach in (IDictionary<String, Object>)student)
+{
+    Console.WriteLine(znach.Value);
+}
+dynamic specialnost = new ExpandoObject();
+Console.Write("Название: ");
+specialnost.Nazvanie = Console.ReadLine() ?? "Нет значения";
+Console.Write("Сокращение: ");
+specialnost.Sokrash = Console.ReadLine() ?? "Нет значения";
+Console.WriteLine("\nДанные специальности: ");
+foreach (var znach in (IDictionary<String, Object>)specialnost)
+{
+    Console.WriteLine(znach.Value);
+}
+dynamic para = new ExpandoObject();
+Console.Write("Время начала пары: ");
+para.NachPar = Console.ReadLine() ?? "Нет значения";
+Console.Write("Время окончания пары: ");
+para.OkonchPar = Console.ReadLine() ?? "Нет значения";
+Console.Write("Время начала перерыва: ");
+para.NachPer = Console.ReadLine() ?? "Нет значения";
+Console.Write("Время окончания перерыва: ");
+para.OkonchPer = Console.ReadLine() ?? "Нет значения";
+//Ссылка на смену -> где-то тут 
+Console.WriteLine("\nДанные пары: ");
+foreach (var znach in (IDictionary<String, Object>)para)
+{
+    Console.WriteLine(znach.Value);
+}
+dynamic smena = new ExpandoObject();
+Console.Write("Название: ");
+smena.Name = Console.ReadLine() ?? "Нет значения";
+Console.WriteLine("\nДанные смены: ");
+foreach (var znach in (IDictionary<String, Object>)smena)
+{
+    Console.WriteLine(znach.Value);
+}
